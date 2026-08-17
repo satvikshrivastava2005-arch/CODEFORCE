7039f4a9a09e33c889501b7f6dc7f34e6ef5efeb
Satvik <satvikshrivastava2005@gmail.com>
2026-08-17 18:14:44 +0530

    Add Watermelon [A] from Codeforces

---
+++ 4/A - Watermelon/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+ 
+    int sum;
+    cin>>sum;
+    if (sum%2==0 && sum>2){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+    return 0;
+}
