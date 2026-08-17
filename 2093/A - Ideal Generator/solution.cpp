e2e867b31450c630bfeb3c14a4ba692760eb4067
Satvik <satvikshrivastava2005@gmail.com>
2026-08-17 18:16:40 +0530

    Add Ideal Generator [A] from Codeforces

---
+++ 2093/A - Ideal Generator/solution.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t,k;
+    cin>>t;
+    for(int i =0 ; i<t;i++){
+        cin>>k;
+        if(k%2==0){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }
+    } 
+}
