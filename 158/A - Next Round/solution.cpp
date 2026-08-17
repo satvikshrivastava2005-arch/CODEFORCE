e7246676c536ca5ade04832003141ee439455759
Satvik <satvikshrivastava2005@gmail.com>
2026-08-17 18:14:53 +0530

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ,k;
+    cin>>n>>k;
+    int arr[n];
+ 
+ for (int i=0 ; i<n;i++){
+    cin>>arr[i];
+ 
+      }
+int next = arr[k-1];
+int count=0;
+    for(int i= 0;i<n; i++){
+       if (arr[i]>=next && arr[i]>0) {
+         count++;
+        }
+    }
+cout<<count<<endl;
+return 0;
+}
