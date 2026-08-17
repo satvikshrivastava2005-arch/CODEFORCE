a5dfc15d5d9bc14276e48f107c29a0d897b6b5b0
Satvik <satvikshrivastava2005@gmail.com>
2026-08-17 18:16:56 +0530

    Add Three Decks [A] from Codeforces

---
+++ 2104/A - Three Decks/solution.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+ 
+        long long sum = a + b + c;
+ 
+        if (sum % 3 == 0 && b <= sum / 3)
+            cout << "YES
+";
+        else
+            cout << "NO
+";
+    }
+ 
+    return 0;
+}
