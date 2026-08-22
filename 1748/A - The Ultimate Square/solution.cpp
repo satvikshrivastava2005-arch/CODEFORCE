f7ba3f701360f28bf13c0dbf301dec4841fcd135
Satvik <satvikshrivastava2005@gmail.com>
2026-08-22 20:08:12 +0530

    Add The Ultimate Square [A] from Codeforces

---
+++ 1748/A - The Ultimate Square/solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+long long  t , n ;
+cin>>t;
+while(t--){
+    cin>>n;
+    cout << (n + 1) / 2 << "
+";
+}
+return 0;
+}
