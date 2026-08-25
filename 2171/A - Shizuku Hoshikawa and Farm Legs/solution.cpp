7d5db2ce3f5ab231f5e12efe146eef422432d62f
Satvik <satvikshrivastava2005@gmail.com>
2026-08-25 07:41:19 +0530

    Add Shizuku Hoshikawa and Farm Legs [A] from Codeforces

---
+++ 2171/A - Shizuku Hoshikawa and Farm Legs/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        if (n % 2 != 0)
+            cout << 0 << '
+';
+        else
+            cout << n /4 +1 << '
+';
+    }
+ 
+    return 0;
+}
