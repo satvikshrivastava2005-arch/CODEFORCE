362edbac2c0bd940e50f5cd06c3724e0246e36d7
Satvik <satvikshrivastava2005@gmail.com>
2026-08-27 20:20:40 +0530

    Add Sum [A] from Codeforces

---
+++ 1742/A - Sum/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t ,a,b,c;
+    cin>>t;
+    while(t--){
+        cin>>a>>b>>c;
+        if(a==b+c || b==a+c || c==a+b){
+            cout<<"YES"<<"
+";
+        }
+        else{
+            cout<<"NO"<<"
+";
+        }
+    }
+}
