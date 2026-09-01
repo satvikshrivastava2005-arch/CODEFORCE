51bb7a74c622e5dc19fe0d0426f71fc31f36669b
Satvik <satvikshrivastava2005@gmail.com>
2026-09-01 18:36:00 +0530

    Add Ultra-Fast Mathematician [A] from Codeforces

---
+++ 61/A - Ultra-Fast Mathematician/solution.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1, s2 , ans="" ;
+    cin >> s1 >> s2;
+    
+   for(int i =0 ; i< s1.size(); i++){
+    if(s1[i]==s2[i]){
+        ans+="0";
+    }
+    else{
+        ans+="1";
+    }
+    }
+    cout<< ans <<endl;
+return 0;
+}
