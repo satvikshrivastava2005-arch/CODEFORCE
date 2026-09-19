6a12bc46af2a854be68b3aabc35ec52f37195c91
Satvik <satvikshrivastava2005@gmail.com>
2026-09-19 09:08:25 +0530

    Add Game With Sticks [A] from Codeforces

---
+++ 451/A - Game With Sticks/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    if (min(n, m) % 2 != 0) {
+        cout << "Akshat
+";
+    } else {
+        cout << "Malvika
+";
+    }
+    
+    return 0;
+}
