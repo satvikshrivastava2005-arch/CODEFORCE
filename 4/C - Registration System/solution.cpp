fa9a2a32c98e018cfe9b43f0a86a0d33c1052e3e
Satvik <satvikshrivastava2005@gmail.com>
2026-09-26 20:08:39 +0530

    Add Registration System [C] from Codeforces

--- 4/C - Registration System/solution.cpp
+++ 4/C - Registration System/solution.cpp
@@ -1,25 +1,24 @@
-#include<iostream>
-#include<map>
-#include<string>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n ;
-    cin>>n;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    unordered_map<string,int> mp;
+    string a;
+    for(int i = 0; i < n; i++){
+        cin >> a;
+        if(mp[a] == 0){
+            cout << "OK
+";
+        }
+        else{
+            cout << a << mp[a] << "
+";
+        }
+        mp[a]++;
+    }
  
- 
- map<string,int> counts;
-for (int i =0;i<n; i++){
-    string name;
-    cin>>name;
- 
-if(counts.count(name)==0){
-cout<<"OK"<<endl;
-counts[name]=1;
-}
-  else{
-  
-    cout<<name<<counts[name]<<endl;
-    counts[name]++;
-  }}
-  return 0 ;
+    return 0;
 }
